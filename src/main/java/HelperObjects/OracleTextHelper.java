d7ccc150cee13b93eb6fe87eaf77978613db0060
Frank <Frankpserra@gmail.com>
2019-07-12 22:21:06 -0400

    Added null checking to card faces array in helpers

--- src/main/java/HelperObjects/OracleTextHelper.java
+++ src/main/java/HelperObjects/OracleTextHelper.java
@@ -12,7 +12,7 @@ public class OracleTextHelper {
     	if(c.oracle_text != null) {
     		return (StringUtils.contains(c.oracle_text, " create") || StringUtils.contains(c.oracle_text, "Create "));
     	}
-    	else if(c.card_faces.size() > 0) {
+    	else if(c.card_faces != null && c.card_faces.size() > 0) {
     		for (CardFace face : c.card_faces) {
 				if(StringUtils.contains(face.oracle_text, " create") || StringUtils.contains(face.oracle_text, "Create "))
 					return true;
@@ -30,11 +30,14 @@ public class OracleTextHelper {
     	if(c.oracle_text != null) {
     		return (StringUtils.countMatches(c.oracle_text, "Create") + StringUtils.countMatches(c.oracle_text, " create"));
     	}
-    	else if(c.card_faces.size() > 0) {
+    	else if(c.card_faces != null && c.card_faces.size() == 2) {
     		return Math.max(
     				StringUtils.countMatches(c.card_faces.get(0).oracle_text, "Create") + StringUtils.countMatches(c.card_faces.get(0).oracle_text, " create"), 
     				StringUtils.countMatches(c.card_faces.get(1).oracle_text, "Create") + StringUtils.countMatches(c.card_faces.get(1).oracle_text, " create")
     				);
+    	}
+    	else if(c.card_faces != null && c.card_faces.size() == 1) {
+    		return StringUtils.countMatches(c.card_faces.get(0).oracle_text, "Create") + StringUtils.countMatches(c.card_faces.get(0).oracle_text, " create");
     	}
     	
     	return 0;
@@ -44,7 +47,7 @@ public class OracleTextHelper {
     	if(c.oracle_text != null) {
     		return (StringUtils.containsIgnoreCase(c.oracle_text, text));
     	}
-    	else if(c.card_faces.size() > 0) {
+    	else if(c.card_faces != null && c.card_faces.size() > 0) {
     		for (CardFace face : c.card_faces) {
 				if(StringUtils.containsIgnoreCase(face.oracle_text, text))
 					return true;
@@ -58,7 +61,7 @@ public class OracleTextHelper {
 		if(c.oracle_text != null) {
     		return c.oracle_text.replace("\r", "").replace("\n", "").toLowerCase().matches(text.toLowerCase());
     	}
-    	else if(c.card_faces.size() > 0) {
+    	else if(c.card_faces != null && c.card_faces.size() > 0) {
     		for (CardFace face : c.card_faces) {
 				if(face.oracle_text.replace("\r", "").replace("\n", "").toLowerCase().matches(text.toLowerCase()))
 					return true;
@@ -74,7 +77,7 @@ public class OracleTextHelper {
 		if(c.oracle_text != null) {
     		return pattern.matcher(c.oracle_text).find();
     	}
-    	else if(c.card_faces.size() > 0) {
+    	else if(c.card_faces != null && c.card_faces.size() > 0) {
     		for (CardFace face : c.card_faces) {
 				if(pattern.matcher(face.oracle_text).find())
 					return true;
